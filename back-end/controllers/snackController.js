755257488d0fe40723414b2656812f11d0e1366c
hinkabi <hinkabi@gmail.com>
2022-02-11 14:52:46 -0500

    create and delete snack

--- back-end/controllers/snackController.js
+++ back-end/controllers/snackController.js
@@ -1,13 +1,12 @@
 const express = require("express");
 const snacks = express.Router();
-const { getAllSnacks, getOneSnack } = require("../queries/snacks.js");
+const { getAllSnacks, getOneSnack , deleteSnack, createSnack} = require("../queries/snacks.js");
 
 snacks.get("/", async (req, res) => {
   try {
     const allSnacks = await getAllSnacks();
     // console.log(allSnacks)
     if (allSnacks[0]) {
-        console.log(allSnacks);
       res.status(200).json(allSnacks);
     } else {
       res.status(500).json({ error: "server error" });
@@ -30,6 +29,30 @@ snacks.get("/:id", async (req,res) =>{
         console.log(err);
     }
 
+});
+
+snacks.delete("/:id", async(req, res)=>{
+    const { id } = req.params;
+    const deletedSnack = await deleteSnack(id);
+    if(deletedSnack.id){
+        res.status(200).json(deletedSnack);
+    } else {
+        res.status(404).json({error: "Snack not found"});
+    }
+});
+
+snacks.post("/", async (req, res)=>{
+    const { body } = req;
+    try{
+        const createdSnack = await createSnack(body);
+        if(createdSnack.id){
+            res.status(200).json(createdSnack);
+        } else {
+            res.status(500).json({error: "Snack creation error"});
+        }
+    } catch(err){
+        console.log(err);
+    }
 })
 
 
--- back-end/queries/snacks.js
+++ back-end/queries/snacks.js
@@ -12,11 +12,31 @@ getAllSnacks = async () =>{
 
 getOneSnack = async (id) =>{
     try{
-        const oneSnack = await db.one("SELECT * snacks WHERE id  =$1", id);
+        const oneSnack = await db.one("SELECT * FROM snacks WHERE id=$1", id);
         return oneSnack;
     }catch(err){
         return err
     }
+};
+
+deleteSnack = async (id) =>{
+    try{
+        const deletedSnack = await db.one(
+            "DELETE FROM snacks WHERE id=$1 RETURNING *", id);
+            return deletedSnack;
+    }catch(err){
+        return err
+    }
+};
+
+createSnack = async (snack) => {
+    try{
+        const newSnack = await db.one ("INSERT INTO snacks( name, image, fiber, protein, added_sugar, is_healthy) VALUES($1, $2, $3, $4, $5, $6) RETURNING *",
+        [snack.name, snack.image, snack.fiber, snack.protein, snack.added_sugar, snack.is_healthy]);
+        return newSnack;
+    }catch (err){
+        return err
+    }
 }
 
-module.exports = {getAllSnacks, getOneSnack};
+module.exports = {getAllSnacks, getOneSnack, deleteSnack, createSnack};
