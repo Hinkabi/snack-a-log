a24ba1ea39d243f620efd7231d9ffea43f214d5a
Larisa <larisashapkinab@gmail.com>
2022-02-13 19:11:21 -0500

    started work on front end

--- front-end/src/Components/HeartHealth.js
+++ front-end/src/Components/HeartHealth.js
@@ -1,11 +1,35 @@
 import heartSolid from "../assets/heart-solid.png";
 import heartOutline from "../assets/heart-regular.png";
+import { Link } from "react-router-dom";
+import axios from "axios";
+import { useState, useEffect } from "react";
+import Snack from "../Components/Snack.js";
+
+const API = process.env.REACT_APP_API_URL;
 
 function HeartHealth({ snackHealth }) {
+
+  const [snacks, setSnacks] = useState([]);
+  useEffect(() => {
+    axios.get(API + "/snacks")
+    .then((res)=>{
+      setSnacks(res.data);
+    }).catch((err)=>{
+      console.log(err);
+    })
+  }, []);
+
   return (
-    <>
-      <p>????</p>
-    </>
+    <div>
+      <button>
+        <Link to="/snacks/new">New Snack</Link>
+      </button>
+      <div>
+        {snacks.map((snack) => {
+        return <Snack key={snack.id} snack={snack}/>;
+        })}
+      </div>
+    </div>
   );
 }
 
